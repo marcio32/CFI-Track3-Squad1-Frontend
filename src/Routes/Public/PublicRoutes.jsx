9cf75e868ac430587077ca63cb3977630195b016
Arturo Blanco <blancoarturo92@gmail.com>
2023-12-17 20:09:06 -0300

    Se corrije redirecciones y rutas publicas

--- src/Routes/Public/PublicRoutes.jsx
+++ src/Routes/Public/PublicRoutes.jsx
@@ -1,5 +1,17 @@
+import { CreateUser } from "../../components/user/CreateUser";
+import { Deposit } from "../../components/account/Deposit";
+import { Login } from "../../components/login/Login";
+
 export const PublicRoutes = () => {
-    return(
-        <h1> Estas en las rutas publicas</h1>
-    )
+
+    const { pathname } = window.location;
+
+    if (pathname == "/register") {
+        return <CreateUser />
+    } else if (pathname == "/depositar") {
+        return <Deposit />
+    } else {
+        return <Login/>
+    }
+    
 }
