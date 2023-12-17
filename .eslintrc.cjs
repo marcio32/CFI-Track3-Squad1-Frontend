191ea845c0948e41aa58ecfdae585d041da48696
etchepared <88214684+etchepared@users.noreply.github.com>
2023-12-16 22:19:52 -0300

    feat: add feat page creacion de usuario

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,6 +13,6 @@ module.exports = {
   plugins: ["react-refresh"],
   rules: {
     "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
-    "no-console": "error",
+    //"no-console": "error",
   },
 };
