b826a446c680eb321269606848ed84b4ed91bacd
Arturo Blanco <blancoarturo92@gmail.com>
2023-12-18 16:06:38 -0300

    Se agrega eliminacion y editado de cuentas a vista user

--- src/components/table/CustomTable.jsx
+++ src/components/table/CustomTable.jsx
@@ -1,8 +1,63 @@
-import { Table } from "react-bootstrap";
-import PropTypes from 'prop-types'
+import { Table, Button } from "react-bootstrap";
 import { formatDate } from "../../helpers/formatedDate";
+import PropTypes from 'prop-types'
+import { useState } from "react";
+import { CustomModal } from "../modal/CustomModal";
+import { deleteAccount, editAccount } from "../../selectors/accountServices.mjs";
+
 
 export const CustomTable = ({ data }) => {
+
+    const [accountId, setAccountId] = useState(null);
+    const [isOpenModal, setIsOpenModal] = useState(false);
+    const [isOpenModal2, setIsOpenModal2] = useState(false);
+    const [deleteResponse, setDeleteResponse] = useState('');
+    const [updateResponse, setUpdateResponse] = useState('');
+    const [accountData, setAccountData] = useState({
+        money: 0,
+        isBlocked: false,
+        userId: 0
+    })
+
+    const handleCloseModal = () => setIsOpenModal(false);
+    const handleCloseModal2 = () => setIsOpenModal2(false);
+
+    const handleDeleteAccount = async () => {
+        try {
+            await deleteAccount(accountId);
+            setDeleteResponse('Se ah eliminado la cuenta.')
+        }
+        catch (error) {
+            setDeleteResponse('Ocurrio un error intente nuevamente por favor.')
+        }
+    }
+
+    const handleUpdateAccount = async () => {
+        try {
+            await editAccount(accountId, accountData);
+            setUpdateResponse('Se ah modificado la cuenta.')
+        }
+        catch (error) {
+            setUpdateResponse('Ocurrio un error intente nuevamente por favor.')
+        }
+    }
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setAccountData({
+            ...accountData,
+            [name]: value,
+        })
+    }
+    console.log(accountData.isBlocked)
+    const handleCheboxChange = (event) => {
+        const { name, checked } = event.target;
+        setAccountData({
+            ...accountData,
+            [name]: checked
+        });
+    }
+
     return (
         <Table striped bordered hover>
             <thead>
@@ -22,12 +77,37 @@ export const CustomTable = ({ data }) => {
                         <td>{e.money}</td>
                         <td>{e.isBlocked ? "Bloqueado" : "Activo"}</td>
                         <td>{e.userId}</td>
+                        <td><Button onClick={() => { setAccountId(e.id), setIsOpenModal2(true) }}>Editar</Button></td>
+                        <td><Button variant="danger" onClick={() => { setAccountId(e.id), setIsOpenModal(true) }}>Baja</Button></td>
                     </tr>
                 ))}
+                <CustomModal isOpen={isOpenModal}
+                    onClose={handleCloseModal}
+                    title="Baja de cuenta"
+                    body={!deleteResponse ? `Va a dar de baja la cuenta numero ${accountId}` : deleteResponse}
+                    onAccept={() => handleDeleteAccount()}
+                />
+                <CustomModal isOpen={isOpenModal2}
+                    onClose={() => { handleCloseModal2(), setUpdateResponse(null) }}
+                    title="Modificacion de cuenta"
+                    body={!updateResponse ?
+                        <>
+                            <label htmlFor="input-money">Dinero</label>
+                            <input id="input-money" type="decimal" placeholder="Dinero" name="money" value={accountData.money} onChange={handleInputChange} />
+                            <label htmlFor="input-checbox">Bloquear</label>
+                            <input id="input-checbox" type="checkbox" name="isBlocked" onChange={handleCheboxChange} />
+                            <label htmlFor="input-user-id">N° Usuario</label>
+                            <input id="input-user-id" type="number" placeholder="Numero usuario" name="userId" value={accountData.userId} onChange={handleInputChange} />
+                        </> :
+                        updateResponse
+                    }
+                    onAccept={() => handleUpdateAccount()}
+                />
             </tbody>
         </Table>
     );
 }
+
 CustomTable.propTypes = {
     data: PropTypes.array.isRequired
 }
--- src/selectors/accountServices.mjs
+++ src/selectors/accountServices.mjs
@@ -2,12 +2,23 @@ import axios from "axios";
 import { urlBase } from "./apiPath";
 
 
-export const getAccount = async(userId) => {
+export const getAccount = async (userId) => {
     const response = await axios.get(`${urlBase}Account/details/${userId}`);
-    return response.data
+    return response.data;
 }
 
-export const getAllAccounts = async() => {
+export const getAllAccounts = async () => {
     const response = await axios.get(`${urlBase}Account`);
-    return response.data
+    return response.data;
+}
+
+export const deleteAccount = async (accountId) => {
+    const response = await axios.delete(`${urlBase}Account/${accountId}`);
+    return response.data;
+}
+
+export const editAccount = async (accountId, accountData) => {
+    console.log(accountData)
+    const response = await axios.put(`${urlBase}Account/${accountId}`, accountData);
+    return response.data;
 }
